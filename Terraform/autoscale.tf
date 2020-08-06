57979646ffea5b201a3b9c91a6628b9724bc71ff
alexandra-neubauer <alexandra-neubauer@pluralsight.com>
2020-08-06 15:50:52 -0600

    add route table and internet gateway

--- Terraform/autoscale.tf
+++ Terraform/autoscale.tf
@@ -1,3 +1,8 @@
+resource "aws_key_pair" "alex_ps" {
+  key_name   = "alex-ps-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCty6EvRgyezTJBZA9DcCHqz1aHphcg0OmwW9T/Al1nsz6HgggGeRwNQ18YLmuJvQV5ueff4FJs4NuxY3Lw7NqJ7H4jYBEJ7WPXgTNrZvL1eYnA5iSioI6xadWJHUbsET6wLxLR8Y3azcwiA0azLMDvprgTUmjjjhj/J541Ql2MeNTLWO/w/8uUwFsiSE8G1hcVFoyxiRR6Yg878PHhOZFtEH9Q1vYe0Dqd61HftdLfMlB7VlTMn9S1zB0/Ho2ZOD7vjiLcNqgAuZRZTvvvi6aOcsI5iS+EnrXSJcv8Fqzrlz6BX/jWvxolO52ZPo5d3K0CYMAUTj92w9886i3ZVePcdykB7kQOBBd8PNFlcjEU5mCYlI0hYZWZhF66eEZf3fgAvG9p3cUCb8m3LapXVYMjkpWR7aC/MCRb7T2hCtmKxqbKXnZyaJSFnJyMBkaVGTI9k7NYtcydJLvd/E+cUwZC8yAQWulbP2BxXEDGf35itGd1o9KuOESjlNix05sgNsM= alexandra-neubauer@AleNeub-MBP"
+}
+
 resource "aws_autoscaling_group" "alexs_scaling" {
   name                      = "alexs-scaling"
   max_size                  = 2
@@ -6,8 +11,9 @@ resource "aws_autoscaling_group" "alexs_scaling" {
   health_check_type         = "ELB"
   desired_capacity          = 1
   launch_configuration      = aws_launch_configuration.alexs_launch.name
-  vpc_zone_identifier       = [aws_vpc.main.id]
+  vpc_zone_identifier       = [aws_subnet.subnet_0.id, aws_subnet.subnet_1.id]
   target_group_arns         = [aws_lb_target_group.alexs-page.arn]
+  # suspended_processes = ["ReplaceUnhealthy"]
   tag {
     key                 = "Name"
     value               = "Alexs Web Server"
@@ -19,7 +25,7 @@ resource "aws_launch_configuration" "alexs_launch" {
   name_prefix   = "alexs-lc-"
   image_id      = "ami-07c1207a9d40bc3bd"
   instance_type = "t2.micro"
-  #key_name      = "alex"
+  key_name      = aws_key_pair.alex_ps.key_name
   user_data     = data.local_file.alexs-user-data.content
   security_groups = [
     aws_security_group.alexs_sg_bc.id
--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -6,13 +6,41 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "alex-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "external_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "alex-external-route-table"
+  }
+}
+
+resource "aws_route" "external_route_0" {
+  route_table_id            = aws_route_table.external_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gateway.id
+}
+
 resource "aws_subnet" "subnet_0" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
   availability_zone = data.aws_availability_zones.available.names[0]
   tags = {
     Name = "alex-subnet-0"
   }
+}
+
+resource "aws_route_table_association" "subnet_0" {
+  subnet_id      = aws_subnet.subnet_0.id
+  route_table_id = aws_route_table.external_route_table.id
 }
 
 resource "aws_subnet" "subnet_1" {
@@ -25,10 +53,11 @@ resource "aws_subnet" "subnet_1" {
   }
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
+resource "aws_route_table_association" "subnet_1" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.external_route_table.id
 }
 
-output "aws_subnet_1-id" {
-  value = aws_subnet.subnet_0.id
+data "aws_availability_zones" "available" {
+  state = "available"
 }
